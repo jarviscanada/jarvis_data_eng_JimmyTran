16c1f67b9bb0948492663218635c9e4ec60ccdd8
Jimmy Vu Tran <38023521+JaayTran@users.noreply.github.com>
2024-01-19 09:39:23 -0500

    began implementing JavaGrepImp file, creating the setters and getters

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,83 @@
+package ca.jrvs.apps.grep;
+
+public class JavaGrepImp implements JavaGrep {
+
+    final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
+
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentsException("USAGE: JavaGrep regex rootPath outFile");
+        }
+
+        //Use default logger config
+
+        BasicConfigurator.configure();
+
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try {
+            javaGrepImp.process();
+        } catch (Exception ex) {
+            javaGrepImp.logger.error("Error: Unable to process", ex)
+        }
+    }
+
+    void process() throws IOException;
+
+    @Override
+    public List<File> listFiles(String rootDir){
+
+    }
+
+    @Override
+    public List<String> readLines(File inputFile){
+
+    }
+
+    @Override
+    public boolean containsPattern(String line){
+
+    }
+
+    @Override
+    public void writeToFile(List<String> lines) throws IOException{
+
+    }
+
+    @Override
+    public String getRootPath() {
+        return rootPath;
+    }
+
+    @Override
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    @Override
+    public String getRegex() {
+        return regex;
+    }
+
+    @Override
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    @Override
+    public String getOutFile() {
+        return outFile;
+    }
+
+    @Override
+    public void setOutFile(String outFile) {
+        this.outFile = outFile;
+    }
+}
