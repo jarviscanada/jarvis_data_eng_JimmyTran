db251f290c0f6992a0e4786e6867a563416eeb94
Jimmy Vu Tran <38023521+JaayTran@users.noreply.github.com>
2024-02-07 10:10:29 -0500

    Started JDBC Learning tickets

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/practice/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package ca.jrvs.apps.jdbc.practice;
+
+public interface SimpleCalculator {
+
+    public int add(int x, int y);
+
+    public int subtract(int x, int y);
+
+    public int multiply(int x, int y);
+
+    public double divide(int x, int y);
+
+}
